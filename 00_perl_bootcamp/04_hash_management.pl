e7bfadd9f0f77fb31d3a7f3c3761a0ebb7c08f59
ren-joey <tsuyiren@gmail.com>
2024-09-10 17:51:17 +0800

    tidy: 🫧 remove some unnecessary files

--- 00_perl_bootcamp/04_hash_management.pl
+++
@@ -1,8 +0,0 @@
-%leaders = ('india' => 'peter', 'taiwan' => 'joey');
-@places = keys %leaders;
-@names = values %leaders;
-print "@places\n@names\n";
-
-while (($key, $val) = each %leaders) {
-    print "$key => $val\n";
-}

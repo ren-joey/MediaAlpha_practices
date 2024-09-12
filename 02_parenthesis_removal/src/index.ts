2adbd12086c359aef86a2a68ac7cf18085f60113
ren-joey <tsuyiren@gmail.com>
2024-09-12 21:05:48 +0800

    upt: ♻️ leading operator solution

--- 02_parenthesis_removal/src/index.ts
+++ 02_parenthesis_removal/src/index.ts
@@ -16,53 +16,75 @@
  * @returns
  */
 
-interface Hash { [s: string]: string };
-
 const replaceRange = (s: string, start: number, end: number, substitute: string) => {
     return s.substring(0, start) + substitute + s.substring(end);
 };
 
-const algebraReplace = (s: string): [expression: string, hash: Hash] => {
-    const hash: Hash = {};
-    let stack: number[] = [];
-    let detected = false;
+const algebraReplace = (s: string): [expression: string, list: string[]] => {
+    const list: string[] = [];
     const negativeNumbers: number[][] = [];
-    let charCode: number = 65;
+
+    let operators: number[] = [];
+    let operands: number[] = [];
+    let leadingOperator = true;
 
     for (let i = 0; i < s.length; i += 1 ) {
         const c = s[i];
-        if (c === '-' && (stack.length === 1 || i === 0)) {
-            stack = [i];
-            detected = true;
+        if(c !== '(' && c !== ')') {
+            if (c === '+' || c === '-' || c === '*' || c === '/') {
+
+                // If there is a operator and still the first operator
+                if (operators.length === 1 && leadingOperator) {
+                    leadingOperator = false;
+
+                    // Check if the operator is "-" and are there already have operands
+                    if (s[operators[0]] === '-' && operands.length > 0) {
+                        negativeNumbers.push([operators[0], operands[operands.length - 1]]);
+                    }
+                    operands = [];
+                    operators = [i];
+                }
+
+                // If there are 2 operators along with some operands
+                // assume the second operator will definitely is '-'
+                else if (operands.length > 0 && operators.length >= 2) {
+                    negativeNumbers.push([operators[1], operands[operands.length - 1]]);
+                    operands = [];
+                    operators = [i];
+
+                // If there already has operands and operator, mean the operator belongs a new operand.
+                } else if (operands.length > 0) {
+                    operands = [];
+                    operators = [i];
+
+                // If there are no operands, directly push the operator
+                } else operators.push(i);
+
+            // Push any operands directly
+            } else {
+                leadingOperator = false;
+                operands.push(i);
+            }
         }
-        else if (!detected && (c === '+' || c === '-' || c === '*' || c === '/')) {
-            stack = [i];
-        }
-        else if (/[0-9]/.test(c) && detected === true) stack.push(i);
-        else if (/[A-Za-z]/.test(c)) {
-            detected = false;
-            stack = [];
-        }
-        else if (detected) {
-            negativeNumbers.push([...stack]);
-            detected = false;
-            stack = [];
-        }
+    }
+
+    if (operands.length > 0 && operators.length >= 2) {
+        negativeNumbers.push([operators[1], operands[operands.length - 1]]);
     }
 
     for (let i = negativeNumbers.length - 1; i >= 0; i -= 1) {
         const nums = negativeNumbers[i];
-        const algebra = String.fromCharCode(charCode);
-        hash[algebra] = s.substring(nums[0], nums[nums.length - 1] + 1);
-        s = replaceRange(s, nums[0], nums[nums.length - 1] + 1, algebra);
-        charCode += 1;
+        list.unshift(s.substring(nums[0], nums[nums.length - 1] + 1));
+        s = replaceRange(s, nums[0], nums[nums.length - 1] + 1, '_');
     }
 
-    return [s, hash];
+    console.log(s);
+
+    return [s, list];
 };
 
 const parenthesisRemoval = (s: string): string => {
-    const [expression, hash] = algebraReplace(s);
+    const [expression, list] = algebraReplace(s);
     const stack: any[] = [];
     const operators: any[] = [];
     const len = expression.length;
@@ -87,8 +109,6 @@ const parenthesisRemoval = (s: string): string => {
                     if (d === '+' || d === '-' || d === '*' || d === '/') hasOperator = true;
                 }
 
-                console.log(addSub ? 'T' : 'F');
-
                 const ind = stack.pop() as number;
                 if (operators.length === 0) {
                     del[i] = 1;
@@ -97,7 +117,6 @@ const parenthesisRemoval = (s: string): string => {
                 }
 
                 const d = expression[operators[operators.length - 1]];
-                console.log(d);
                 if (
                     (d === '+' || ((d === '*' || d === '-' || d === '/') && !addSub))
                     || ((d === '*' || d === '/') && innerPrior && addSub)
@@ -121,9 +140,10 @@ const parenthesisRemoval = (s: string): string => {
     }
 
     for (let i = ans.length - 1; i >= 0; i -= 1) {
-        if (hash[ans[i]]) {
-            if (i !== 0) ans = replaceRange(ans, i, i+1, `(${hash[ans[i]]})`);
-            else ans = replaceRange(ans, i, i+1, `${hash[ans[i]]}`);
+        if (ans[i] === '_') {
+            const exp = list.pop();
+            if (i !== 0 && ans[i-1] !== '(') ans = replaceRange(ans, i, i+1, `(${exp})`);
+            else ans = replaceRange(ans, i, i+1, `${exp}`);
         }
     }
 
